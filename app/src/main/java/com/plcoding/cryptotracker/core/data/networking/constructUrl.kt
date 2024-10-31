c75ce14c499b2a9073738ec48ee609d46d921ccd
brandon duran <duranbrandon927@gmail.com>
2024-10-31 20:36:43 +0800

    add safecall function

--- app/src/main/java/com/plcoding/cryptotracker/core/data/networking/constructUrl.kt
+++ app/src/main/java/com/plcoding/cryptotracker/core/data/networking/constructUrl.kt
@@ -2,6 +2,7 @@ package com.plcoding.cryptotracker.core.data.networking
 
 import com.plcoding.cryptotracker.BuildConfig
 
+
 fun constructUrl(url: String): String {
     return when {
         url.contains(BuildConfig.BASE_URL) -> url
